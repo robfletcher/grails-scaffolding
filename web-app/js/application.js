0eea86e87debfb37e18a6e137cf8f49e66dbef44
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-01-01 23:01:03 +0000

    WIP javascript stuff

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -17,11 +17,22 @@ $(document).ready(function() {
 		// decorate list pagination & sorting controls with AJAX
 		$(".pagination a, th.sortable a").live("click", function() {
 			var url = $(this).attr("href");
-			$(".content").load(url + " .content");
+			$(".content").load(url + " .content", function(response) {
+				history.pushState({html: $(".content").html()}, "");
+			});
 			return false;
 		});
+
+		window.onpopstate = function(event) {
+			if (event.state) {
+				$(".content").replaceWith(event.state.html);
+			}
+		};
 	}
 
 	// prevent FOUC by only making body visible once document is ready
 	$("body").addClass("ready");
+
+	// focus first error field (this only works once page content is visible)
+	$(".error:first").find("input, select, textarea").focus();
 });
