94b2d747cf9373c88bab1257257a63121f18b985
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-02-13 09:14:25 +0000

    tidied comments

--- web-app/js/grails.autocomplete.js
+++ web-app/js/grails.autocomplete.js
@@ -11,9 +11,9 @@
 				id: select.attr('id') + '-autocompleter',
 				type: 'search'
 			}).autocomplete({
+				// the data source for the autocompleter is all the unselected options from the select filtered by the entered text
 				source: function(ui, callback) {
 					var term = ui.term.toLowerCase();
-					// get all unselected options and filter by the search term entered
 					var options = select.find('option:not(:selected)').filter(function() {
 						return $(this).text().toLowerCase().indexOf(term) != -1;
 					}).map(function(index, option) {
@@ -21,19 +21,24 @@
 					});
 					callback(options);
 				},
+				// when an autocomplete suggestion is chosen the corresponding option in the select is selected
 				select: function(event, ui) {
-					// select the corresponding option in the original select element
 					var matchingOption = select.find('option[value=' + ui.item.id + ']');
 					matchingOption.attr('selected', true);
 					select.trigger('change');
-					// clear the search input and prevent default behaviour that completes the term
+					// clear the search input and prevent default behaviour
 					$(this).val('');
 					return false;
 				}
 			});
 
 			// create an output element that mirrors the content of the select when it changes
-			var output = $('<div/>', { id: select.attr('id') + '-output', class: 'output' }).data('for', select.attr('id')).html("<ul></ul>");
+			var output = $('<div/>', { 
+				id: select.attr('id') + '-output', 
+				class: 'output' 
+			}).data('for', select.attr('id')).html("<ul></ul>");
+			
+			// whenever selection changes (whether due to autocompleter or any other trigger) update the output list
 			select.bind('change', function() {
 				var outputItems = output.find('li');
 				$(this).find('option').each(function(index) {
@@ -54,7 +59,7 @@
 				});
 			});
 
-			// each selected element has a button that can be used to remove it from the selection
+			// each element in the output list has a button that can be used to remove it from the selection
 			$('.autocomplete-delete-button').live('click', function() {
 				var objectId = $(this).parent('li').data('object-id');
 				select.find('option[value=' + objectId + ']').attr('selected', false);
