d643f646dfc96e1a001d0770c50607b71fefeb03
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-03-27 17:02:14 +0100

    apply maxlength to textarea as well as input type=text

--- test/functional/scaffolding/InputTypesSpec.groovy
+++ test/functional/scaffolding/InputTypesSpec.groovy
@@ -61,6 +61,7 @@ class InputTypesSpec extends NoJavascriptSpec {
 		where:
 		input                  | attribute   | value
 		"#name"                | "maxlength" | "40"
+		"#longText"            | "maxlength" | "251"
 		"#textWithMatches"     | "pattern"   | /\w+/
 		"#numberWithMin"       | "min"       | "0"
 		"#numberWithMax"       | "max"       | "10"
