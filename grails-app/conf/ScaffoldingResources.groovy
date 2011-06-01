80421a5d0252759df3a31a2ef8ac8ad7970e0a5a
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-06-01 13:32:21 +0100

    split CSS files out

--- grails-app/conf/ScaffoldingResources.groovy
+++ grails-app/conf/ScaffoldingResources.groovy
@@ -6,6 +6,7 @@ modules = {
 		dependsOn "modernizr", "jquery"
 		resource "js/grails.ajaxloader.js"
 		resource "js/grails.ajaxloader.loader.js"
+		resource "css/grails.ajaxloader.css"
 	}
 	
 	// an autocomplete control to replace multiple-select inputs for one-to-many and many-to-many relationships
@@ -14,6 +15,7 @@ modules = {
 		dependsOn "modernizr", "jquery"
 		resource "js/grails.autocomplete.js"
 		resource "js/grails.autocomplete.loader.js"
+		resource "css/grails.autocomplete.css"
 	}
 	
 	// AJAX-enables scaffolded list pages
@@ -30,6 +32,7 @@ modules = {
 		dependsOn "modernizr", "jquery"
 		resource "js/grails.range.js"
 		resource "js/grails.range.loader.js"
+		resource "css/grails.range.css"
 	}
 	
 	// replaces standard error messages with tooltips attached to each individual field
@@ -38,6 +41,7 @@ modules = {
 		dependsOn "modernizr", "jquery"
 		resource "js/grails.errors.js"
 		resource "js/grails.errors.loader.js"
+		resource "css/grails.errors.css"
 	}
 	
 	// replaces flash and error messages with a fade-out message
@@ -46,6 +50,7 @@ modules = {
 		dependsOn "modernizr", "jquery"
 		resource "js/grails.alerts.js"
 		resource "js/grails.alerts.loader.js"
+		resource "css/grails.alerts.css"
 	}
 	
 	// some basic form enhancements
@@ -67,7 +72,6 @@ modules = {
     "scaffolding" {
 		defaultBundle "scaffolding"
 		dependsOn "grails.ajax.loader", "grails.autocomplete", "grails.list", "grails.range", "grails.errors", "grails.alerts", "grails.forms", "grails.fouc"
-		resource "css/enhanced-scaffolding.css"
 	}
 	
 }
